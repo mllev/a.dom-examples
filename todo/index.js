25d0312b3e557eb4c9ad304598247f2144f97b57
Matthew Levenstein <matthewlevenstein@gmail.com>
2020-01-11 21:56:37 -0700

    latest

--- todo/index.js
+++ todo/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
-const Adom = require('adom-js');
+const Adom = require('../../a.dom/index.js');
 const bodyParser = require('body-parser');
 
 const app = express();
-const adom = new Adom({ rootDir: '.' });
+const adom = new Adom({ rootDir: '.', cache: false });
 
-let todos = [];
+let todos = [
+  'filters',
+  'tree diffing',
+  'async rendering'
+];
 
 app.use(bodyParser.json());
 
@@ -15,9 +19,17 @@ app.post('/item', function (req, res) {
   res.sendStatus(200);
 });
 
+app.delete('/item/:id', function (req, res) {
+  let id = parseInt(req.params.id);
+  todos.splice(id, 1);
+  res.sendStatus(200);
+})
+
 app.use('/', function (req, res) {
   res.set('Content-type', 'text/html');
   res.end(adom.render('index.adom', { items: todos }));
 });
 
-http.createServer(app).listen(5000);
+http.createServer(app).listen(5000, function () {
+  console.log('Listening on port 5000...');  
+});
